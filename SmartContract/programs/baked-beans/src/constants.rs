11fe75a20c841cb3dfa8f6681bd269ac82a7ca10
kundan1911 <83800542+kundan1911@users.noreply.github.com>
2024-01-08 01:56:39 +0530

    Delete SmartContract directory

--- SmartContract/programs/baked-beans/src/constants.rs
+++
@@ -1,5 +0,0 @@
-pub const GLOBAL_STATE_SEED: &[u8] = b"GLOBAL_STATE_SEED";
-
-pub const USER_STATE_SEED: &[u8] = b"USER_STATE_SEED";
-
-pub const VAULT_SEED: &[u8] = b"VAULT_SEED";
--- SmartContract/programs/baked-beans/src/states/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod global_state;
-pub use global_state::*;
-
-pub mod user_state;
-pub use user_state::*;
